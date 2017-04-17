39314f2788cd5e0de7dc32e088aa33013eb4479b
Andrew Malta <andrewsaxx@gmail.com>
2017-04-17 18:36:21 -0300

    Algumas mudanças

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>inConcert</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/reset.css" />
     <link rel="stylesheet" href="assets/css/style.css" />
   </head>
@@ -14,6 +15,13 @@
           inConcert
         </div>
       </header>
+
+      <aside class="menu">
+        <a href="#">Feed de eventos</a>
+        <a href="#">Perfil</a>
+        <a href="#">Sua agenda</a>
+        <a href="#">Configurações</a>
+      </aside>
     </main>
   </body>
 </html>
