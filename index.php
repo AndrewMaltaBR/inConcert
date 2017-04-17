b1fc26e0706ef9bf42bc533792bfba0e13cb9b37
Andrew Malta <andrewsaxx@gmail.com>
2017-04-17 12:04:50 -0300

    Criando estilo base

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>inConcert</title>
-    <link rel="stylesheet" href="assets/reset.css" />
-    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="assets/css/reset.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
   </head>
   <body>
-    
+    <main class="content">
+      <header class="header" background-color="primary">
+        <div class="logo">
+          inConcert
+        </div>
+      </header>
+    </main>
   </body>
 </html>
