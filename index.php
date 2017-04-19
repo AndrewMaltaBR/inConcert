76af38dbe7b9f6d7a830ef0c0d2eccd8a1b6ad30
Andrew Malta <andrewsaxx@gmail.com>
2017-04-19 11:02:59 -0300

    Alterações

--- index.php
+++ index.php
@@ -27,20 +27,38 @@
 
         <section class="card">
           <header class="card-header">
-            <div class="title">Equipe AnimeGO!</div>
-            <button class="btn btn-follow"><i class="fa fa-check"></i>&nbsp;&nbsp;Seguindo</button>
+            <div class="profile-img" style="background-image: url('assets/img/profile-1.jpg')"></div>
+            <div class="profile">Equipe AnimeGO!</div>
+            <button class="btn bordered btn-follow"><i class="fa fa-check"></i>&nbsp;&nbsp;Seguindo</button>
           </header>
-          <div class="card-body"><img src="assets/img/animego.jpg" /></div>
-          <footer class="card-footer">Unoesc</footer>
+          <div class="card-body">
+            <img src="assets/img/animego.jpg" />
+          </div>
+          <footer class="card-footer">
+            <div class="title">AnimeGO!</div>
+            <div class="options">
+              <button class="btn rounded option"><i class="fa fa-heart-o"></i>&nbsp;32.156</button>
+              <button class="btn rounded option"><i class="fa fa-comments"></i>&nbsp;432</button>
+              <button class="btn rounded option"><i class="fa fa-share"></i>&nbsp;432</button>
+            </div>
+          </footer>
         </section>
 
         <section class="card">
           <header class="card-header">
-            <div class="title">Central de eventos Chapecó</div>
-            <button class="btn btn-follow"><i class="fa fa-plus"></i>&nbsp;&nbsp;Seguir</button>
+            <div class="profile-img" style="background-image: url('assets/img/profile-2.jpg')"></div>
+            <div class="profile">Centro de eventos Chapecó</div>
+            <button class="btn bordered btn-follow"><i class="fa fa-plus"></i>&nbsp;&nbsp;Seguir</button>
           </header>
           <div class="card-body"><img src="assets/img/orquestra.jpg" /></div>
-          <footer class="card-footer">Unoesc</footer>
+          <footer class="card-footer">
+            <div class="title">Orquestra de câmara de  San Diego</div>
+            <div class="options">
+              <button class="btn rounded option"><i class="fa fa-heart-o"></i>&nbsp;32.156</button>
+              <button class="btn rounded option"><i class="fa fa-comments"></i>&nbsp;432</button>
+              <button class="btn rounded option"><i class="fa fa-share"></i>&nbsp;432</button>
+            </div>
+          </footer>
         </section>
 
       </article>
